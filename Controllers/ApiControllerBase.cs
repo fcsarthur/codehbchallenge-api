27993352209f8ee382169ab5ab5a220725188451
Arthur Cardoso <cardosovha@gmail.com>
2021-12-18 16:44:01 -0300

    #1 - base controller

---
+++ Controllers/ApiControllerBase.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace codehbchallenge_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public abstract class ApiControllerBase<T> : ControllerBase
+    {
+        private readonly ILogger<T> _logger;
+
+        public ApiControllerBase(ILogger<T> logger)
+        {
+            _logger = logger;
+        }
+
+        
+    }
+}
