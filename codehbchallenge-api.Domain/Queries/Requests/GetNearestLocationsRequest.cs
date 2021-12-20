ac87e00e0d6f9df9b9f1b19acf634b100aed0574
Arthur Cardoso <>
2021-12-20 04:38:33 -0300

    #13 - order by distance

---
+++ codehbchallenge-api.Domain/Queries/Requests/GetNearestLocationsRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace codehbchallenge_api.Domain.Queries.Requests
+{
+    public class GetNearestLocationsRequest
+    {
+        public decimal lat { get; set; }
+        public decimal lng { get; set; }
+    }
+}
